c8cadd498aa765a698e2bcbcb036abe58f382124
Jorge Lobo <jorgelobo04@gmail.com>
2013-03-25 14:53:50 +0100

    Finish user signup

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -52,6 +52,16 @@ describe User do
     it { should_not be_valid }
   end
 
+  describe "email address with different case" do
+    let(:mixed_case_email){ "foo@EXAMPLe.CoM"}
+    it "should be saved as lower case" do
+      @user.email = mixed_case_email
+      @user.save
+      @user.reload.email.should == mixed_case_email.downcase
+    end
+  end
+
+
   describe "when password is not present" do
   before { @user.password = @user.password_confirmation = " " }
   it { should_not be_valid }
@@ -77,4 +87,6 @@ describe User do
       specify { user_for_invalid_password.should be_false }
     end
   end
+
+
 end
