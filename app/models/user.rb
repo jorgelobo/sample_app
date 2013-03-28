c99a8cc86654047c4153be3ea82540a381f03a3d
Jorge Lobo <jorgelobo04@gmail.com>
2013-03-28 14:30:44 +0100

    Finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,8 @@ class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
   has_secure_password
 
-  before_save { email.downcase! }
+  before_save { |user| user.email = email.downcase }
+  before_save :create_remember_token
 
   validates :name,  presence: true, length: { maximum: 50 }
   validates :password, presence: true, length: { minimum: 6 }
@@ -23,4 +24,11 @@ class User < ActiveRecord::Base
   validates :email, presence: true, 
   					format: { with: VALID_EMAIL_REGEX },
   					uniqueness: true, uniqueness: { case_sensitive: false }
+
+
+  private
+
+    def create_remember_token
+      self.remember_token = SecureRandom.urlsafe_base64
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 SampleApp::Application.routes.draw do
-  resources :users
 
-  root to: 'static_pages#home'
+root to: 'static_pages#home'
+
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 
   match '/signup',  to: 'users#new'
-
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
+  
   match '/signup',  to: 'users#new'
 
   match '/help', to: 'static_pages#help'
